95d243fa836f83b69695fab270222e2586335d55
Siyuan Zeng <siyuanzeng@gmail.com>
2015-08-27 03:46:34 -0400

    Lo4j ReviewValidator

--- src/com/mindtree/mcse/mobilemall/web/validators/ReviewValidator.java
+++ src/com/mindtree/mcse/mobilemall/web/validators/ReviewValidator.java
@@ -1,22 +1,22 @@
 package com.mindtree.mcse.mobilemall.web.validators;
 
-import java.util.Date;
-
+import org.apache.log4j.Logger;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
-import com.mindtree.mcse.mobilemall.domain.Order;
-import com.mindtree.mcse.mobilemall.domain.Review;
 import com.mindtree.mcse.mobilemall.domain.hibernateannotation.HReview;
 
 public class ReviewValidator implements Validator {
+	private Logger logger = Logger.getLogger(ReviewValidator.class);
+	
 	@SuppressWarnings("rawtypes")
 	public boolean supports(Class clazz) {
 		return HReview.class.isAssignableFrom(clazz);
 	}
 
 	public void validate(Object obj, Errors errors) {
+		logger.debug("validate() method to validate " + obj.getClass().toString() + " ************************************* ");
 //		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "id", "required.id", "id is required.");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "required.name", "name is required.");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "itemId", "required.itemId", "ItemId is required.");
